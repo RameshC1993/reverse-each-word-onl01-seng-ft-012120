9c70edacd71da5f1b283b86ff39efceed3120c11
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-26 23:45:57 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(string)
+  array = string.split(' ')
+  new_arr = []
+end
+
+
+# def reverse_each_word(string)
+#   array = string.split(' ')
+#   array.collect do |word|
+# end
+#   array.reverse
+# end
